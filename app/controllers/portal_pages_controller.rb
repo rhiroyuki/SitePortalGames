35996b988bd9b0f3ad9073fe2fbcbc2a0e8a1435
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-13 22:47:58 -0300

    Add staticpages controller

---
+++ app/controllers/portal_pages_controller.rb
@@ -0,0 +1,13 @@
+class PortalPagesController < ApplicationController
+  def home
+  end
+
+  def pc
+  end
+
+  def esports
+  end
+
+  def tech
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'portal_pages/home'
+
+  get 'portal_pages/pc'
+
+  get 'portal_pages/esports'
+
+  get 'portal_pages/tech'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
